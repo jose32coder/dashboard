1ca8efba4a8c6c1d30700d31ae2fd7d73503b374
José Manuel <81641564+jose32coder@users.noreply.github.com>
2022-08-16 00:19:49 -0400

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,7 +1,14 @@
 const themeToggler = document.querySelector('.theme-toggler');
+const themeTogglerMobile = document.querySelector('.theme-toggler-mobile');
 themeToggler.addEventListener('click', () =>{
     document.body.classList.toggle('dark-theme-variables');
 
     themeToggler.querySelector('span:nth-child(1)').classList.toggle('active');
     themeToggler.querySelector('span:nth-child(2)').classList.toggle('active')
+})
+themeTogglerMobile.addEventListener('click', () =>{
+    document.body.classList.toggle('dark-theme-variables');
+
+    themeTogglerMobile.querySelector('span:nth-child(1)').classList.toggle('active');
+    themeTogglerMobile.querySelector('span:nth-child(2)').classList.toggle('active')
 })
