35ca65bed2ff6c03f2b4c826afb169c224b4d645
José Manuel <81641564+jose32coder@users.noreply.github.com>
2022-08-15 23:36:05 -0400

    Add files via upload

---
+++ js/arrayej.js
@@ -0,0 +1,44 @@
+const Orders = [   
+{
+    productName: 'Mica Samsung S10',
+    productNumber: '81510',
+    paymentStatus: 'Due', 
+    shipping: 'Pending' 
+},
+{
+    productName: 'Botones encendido y volumen',
+    productNumber: '85135',
+    paymentStatus: 'Refunded', 
+    shipping: 'Declined' 
+},
+{
+    productName: 'Pantalla iPhone X',
+    productNumber: '10101',
+    paymentStatus: 'Due', 
+    shipping: 'Pending' 
+},
+{
+    productName: 'Pin de carga Huawei P30',
+    productNumber: '68898',
+    paymentStatus: 'Paid', 
+    shipping: 'Delivered' 
+},
+{
+    productName: 'Procesador Snapdragon 980S',
+    productNumber: '23145',
+    paymentStatus: 'Paid', 
+    shipping: 'Delivered' 
+},
+{
+    productName: 'Chip de conexion de señal',
+    productNumber: '22354',
+    paymentStatus: 'Due', 
+    shipping: 'Pending' 
+},
+{
+    productName: 'GPS iPhone 13 Pro Max',
+    productNumber: '89645',
+    paymentStatus: 'Refunded', 
+    shipping: 'Declined' 
+}
+]
